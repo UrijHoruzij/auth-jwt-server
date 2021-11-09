c3dadfbf86efb3441dce67d678dc38e200c8f944
Юрий Хоружий <urijhoruzij@gmail.com>
2021-11-09 16:06:57 +0300

    Add mysql database

--- core/db.js
+++ core/db.js
@@ -1,14 +1,25 @@
-const mongoose = require("mongoose");
-const { DB_HOST, MONGOOSE_CONFIG} = require("../config");
+const mysql = require('mysql2');
+const mongoose = require('mongoose');
+const { DB_TYPE, DB_MYSQL_CONFIG, DB_MONGODB_CONFIG } = require('../config');
 
 const db = {};
 db.connect = async () => {
-  const connect = await mongoose.connect(DB_HOST, MONGOOSE_CONFIG);
-  if (connect) {
-    console.log("Успешно подключение к MongoDB");
-  } else {
-    console.error("Ошибка поключения к базе данных");
-  }
+	try {
+		let connect;
+		switch (DB_TYPE) {
+			case 'mysql':
+				connect = mysql.createConnection(DB_MYSQL_CONFIG);
+				if (connect) console.log('Successfully connecting to Mysql');
+			case 'mongodb':
+				connect = await mongoose.connect(DB_MONGODB_CONFIG.host, DB_MONGODB_CONFIG.config);
+				if (connect) console.log('Successfully connecting to MongoDB');
+			default:
+				connect = await mongoose.connect(DB_MONGODB_CONFIG.host, DB_MONGODB_CONFIG.config);
+				if (connect) console.log('Successfully connecting to MongoDB');
+		}
+	} catch {
+		console.error('Database connection error');
+	}
 };
 
 module.exports = db;
